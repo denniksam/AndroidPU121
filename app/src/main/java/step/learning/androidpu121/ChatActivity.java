74703009ab185e86b93a4102e049e9f38847199a
Denys Samoilenko <denniksam@gmail.com>
2023-10-24 11:47:23 +0300

    Chat -- 2

--- app/src/main/java/step/learning/androidpu121/ChatActivity.java
+++ app/src/main/java/step/learning/androidpu121/ChatActivity.java
@@ -2,9 +2,13 @@ package step.learning.androidpu121;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.NetworkOnMainThreadException;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.io.ByteArrayOutputStream;
@@ -12,24 +16,77 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import step.learning.androidpu121.orm.ChatMessage;
+import step.learning.androidpu121.orm.ChatResponse;
 
 public class ChatActivity extends AppCompatActivity {
     private final String chatUrl = "https://chat.momentfor.fun/";
-    private TextView tvTitle ;
+    private LinearLayout chatContainer;
+    private final List<ChatMessage> chatMessages = new ArrayList<>() ;
+
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_chat );
-        tvTitle = findViewById( R.id.chat_tv_title ) ;
+        chatContainer = findViewById( R.id.chat_layout_container ) ;
 
         new Thread( this::loadChatMessages ).start() ;
     }
+    private void showChatMessages() {
+        for( ChatMessage chatMessage : chatMessages ) {
+            chatContainer.addView( chatMessageView( chatMessage ) ) ;
+        }
+    }
+    private View chatMessageView( ChatMessage chatMessage ) {
+        LinearLayout messageContainer = new LinearLayout( ChatActivity.this ) ;
+        messageContainer.setOrientation( LinearLayout.VERTICAL ) ;
+        LinearLayout.LayoutParams containerParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        ) ;
+        messageContainer.setLayoutParams( containerParams ) ;
+
+        TextView tv = new TextView( ChatActivity.this ) ;
+        tv.setText( chatMessage.getAuthor() ) ;
+        tv.setTypeface( null, Typeface.BOLD ) ;
+        messageContainer.addView( tv ) ;
+
+        tv = new TextView( ChatActivity.this ) ;
+        tv.setText( chatMessage.getText() ) ;
+        tv.setTypeface( null, Typeface.ITALIC ) ;
+        messageContainer.addView( tv ) ;
+
+        return messageContainer ;
+    }
+    /*
+    Д.З. Створити дизайн для відображення повідомлень двох типів:
+    "свої" - вирівнювання праворуч, свої кольори, рамка НЕ скруглена у правому-нижньому куті
+    "інші" - по лівому краю, нескруглений кут лівий-нижній
+    Додати виведення часу повідомлення (*) в інтелектуальній формі: якщо сьогодні то
+    тільки час, якщо на день раніше - "учора", інакше повна дата-час
+    Для випробування вивести різні стилі через один
+     */
+
     private void loadChatMessages() {
         try( InputStream inputStream = new URL( chatUrl ).openStream() ) {
-            String body = streamToString( inputStream ) ;
-            runOnUiThread(
-                    () -> tvTitle.setText( body )
-            );
+            ChatResponse chatResponse = ChatResponse.fromJsonString(
+                    streamToString( inputStream ) ) ;
+
+            // Перевіряємо на нові повідомлення, оновлюємо (за потреби) колекцію chatMessages
+            boolean wasNewMessages = false ;
+            for( ChatMessage message : chatResponse.getData() ) {
+                if( chatMessages.stream().noneMatch( m -> m.getId().equals( message.getId() ) ) ) {
+                    // це нове повідомлення (немає у колекції)
+                    chatMessages.add( message ) ;
+                    wasNewMessages = true ;
+                }
+            }
+            if( wasNewMessages ) {
+                runOnUiThread( this::showChatMessages );
+            }
         }
         catch( NetworkOnMainThreadException ignored ) {
             Log.e( "loadChatMessages", "NetworkOnMainThreadException" ) ;
--- app/src/main/java/step/learning/androidpu121/orm/ChatMessage.java
+++ app/src/main/java/step/learning/androidpu121/orm/ChatMessage.java
@@ -33,4 +33,28 @@ public class ChatMessage {
     public void setId( String id ) {
         this.id = id;
     }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor( String author ) {
+        this.author = author;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText( String text ) {
+        this.text = text;
+    }
+
+    public String getMoment() {
+        return moment;
+    }
+
+    public void setMoment( String moment ) {
+        this.moment = moment;
+    }
 }
